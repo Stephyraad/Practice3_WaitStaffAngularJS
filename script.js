b7ace44bf649d7e7fc3e5de222aa762a3f2bd7bd
Stephanie Raad <stephy.raad@gmail.com>
2014-10-14 14:12:21 -0700

    begining of script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 var myApp= angular.module("myApp", []);
+
+myApp.controller("MyController", function($scope){
+	
+	$scope.submit= function subtotal(a, b){
+			var subtotal = a *(1+ b);
+			return subtotal;
+			};
+		function tip(a,b){
+			var tip = a * b/100;
+			return tip;
+			};
+		function total (a,b){
+			return a +b;
+		};
+});
