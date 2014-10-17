e706f29c4daa2e8ae6e4df712919af7739ed7fa0
Stephanie Raad <stephy.raad@gmail.com>
2014-10-16 18:09:19 -0700

    mid way commit

--- script.js
+++ script.js
@@ -1,16 +1,74 @@
 var myApp= angular.module("myApp", []);
 
 myApp.controller("MyController", function($scope){
-	
-	$scope.submit= function subtotal(a, b){
-			var subtotal = a *(1+ b);
+
+	$scope.submit= function(){
+			$scope.subtotal = function(a, b){
+			a *(1+ b);
+			console.log("workoinf")
 			return subtotal;
-			};
-		function tip(a,b){
-			var tip = a * b/100;
+		};
+
+			$scope.tip = function(a,b){
+			a * b/100;
 			return tip;
 			};
-		function total (a,b){
+
+			$scope.total= function (subtotal,tip){
 			return a +b;
 		};
+	}
+
 });
+
+
+
+
+
+// $scope.totTip={};
+	
+	// $scope.submit= function (a, b){
+	// 		 var subtotal = function(a, b){
+	// 		a *(1+ b);
+	// 		console.log("workoinf")
+	// 		return subtotal;
+	// 	};
+		
+	// 		$scope.tip = function(a,b){
+	// 		a * b/100;
+	// 		return tip;
+	// 		};
+
+	// 		$scope.total= function (a,b){
+	// 		return a +b;
+	// 	};
+	// }
+
+
+	
+		// function totalTip (tip){
+		// tip.push($scope.totTip);
+		// return $scope.totTip;
+
+		// };
+
+// // 	$scope.submit= 
+// 		function subtotal(a, b){
+// 			var subtotal = a *(1+ b);
+// 			console.log("workoinf")
+// 			return subtotal;
+// 			};
+// 		function tip(a,b){
+// 			var tip = a * b/100;
+// 			return tip;
+// 			};
+// 		function total (a,b){
+// 			return a +b;
+// 		};
+
+// 		function totalTip (tip){
+// 		tip.push($scope.totTip);
+// 		return $scope.totTip;
+
+// 		};
+// });
