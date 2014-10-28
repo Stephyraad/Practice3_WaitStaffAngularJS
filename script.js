5427d4c02d8e211721bea3b57178fc3149772d95
Stephanie Raad <stephy.raad@gmail.com>
2014-10-28 10:54:16 -0700

    Added routes

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-var myApp= angular.module("myApp", []);
+var myApp= angular.module("myApp", ["ngRoute"]);
+
+myApp.config(function($routeProvider){
+	$routeProvider.when("/", {
+		templateUrl: "./home.html",
+		controller: "HomeCtrl"
+		}).when("/new-meal", {
+			templateUrl: "./newmeal.html",
+			controller: "MealCtrl"
+		}).when("/my-earnings", {
+			templateUrl: "./myearnings.html",
+			controller: "EarningCtrl"
+		});
+	})
+	.controller("HomeCtrl", function($scope){
+		// empty for now
+	})
+	.controller("MealCtrl", function($scope){
+
+	})
+	.controller("EarningCtrl", function($scope){
+
+	});
+
 
 myApp.controller("MyController", function($scope){
 	$scope.mealCount=[];
@@ -50,31 +73,33 @@ myApp.controller("MyController", function($scope){
 
 	}
 
+	//why when i add the .toFixed(2) to z it gives me undefined
+
 	$scope.reset= function(){
 		$scope.x=0;
 		$scope.y= 0;
 		$scope.z=0;
 	}
-
-
 	$scope.resetTwo= function(){
+
+		$scope.mealPrice= null;
+		$scope.taxRate=null;
+		$scope.tipPercent=null;
+
 
 		$scope.x=0;
 		$scope.y= 0;
 		$scope.z=0;
 
 		$scope.a =0;
+		$scope.b =0;
+		$scope.c =0;
 		var tipArray=[];
 		
 
 		$scope.mealCount=[];
 		$scope.b=0;
 	}
-
-
-	
-
-
 });
 
 // NG INIT 
